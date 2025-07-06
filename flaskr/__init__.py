419b86fb654c969020c206d16fd1777c7f7d28ee
GeorgG <georg.goetz@haufe-lexware.com>
2025-07-06 10:22:46 +0200

    Changed branch for deployment pipeline from main to master

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,12 +4,12 @@ from flask import Flask
 
 def create_app(test_config=None):
   app = Flask(__name__, instance_relative_config=True)
-  
+
   secret_key = os.environ.get("SECRET_KEY", "dev")
   db_url = os.environ.get("DATABASE_URL")
   if db_url is None and test_config is None:
     db_url = 'sqlite:///' + os.path.join(app.instance_path, 'flaskr.sqlite')
-  
+
   app.config.from_mapping(
     SECRET_KEY=secret_key,
     SQLALCHEMY_DATABASE_URI=db_url,
