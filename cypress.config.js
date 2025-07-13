4a8ee8f461aa8d83a3a0c5bafc72ef3130882009
GeorgG <georg.goetz@haufe-lexware.com>
2025-07-13 13:27:18 +0200

    feat(cypress): Fixed eslint warnings #24

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,7 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5000',
-    setupNodeEvents(on, config) {
-    },
-  },
-});
+    baseUrl: 'http://localhost:5000'
+  }
+})
